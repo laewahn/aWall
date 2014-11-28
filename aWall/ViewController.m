72ef35b91929f11cadf42836c5c7953e74e6cda7
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2014-11-28 13:37:04 +0100

    Added loading and parsing of IVU data

--- aWall/ViewController.m
+++ aWall/ViewController.m
@@ -14,7 +14,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // Do any additional setup after loading the view.
     NSArray* stubDepartures = @[
                                 @{
                                     @"line": @"33",
@@ -29,12 +28,58 @@
                                 ];
     
     [self setRepresentedObject:stubDepartures];
+    
+    [self loadAndLogDepartures];
 }
 
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
+- (void)loadAndLogDepartures
+{
+    NSURLSession* departuresDownloadSession = [NSURLSession sharedSession];
+    
+    NSURL* departuresEndpointURL = [NSURL URLWithString:@"http://ivu.aseag.de/interfaces/ura/instant_V1?ReturnList=LineID,Latitude,Longitude,DestinationText,EstimatedTime&StopID=100625"];
+    NSURLRequest* departuresInfoRequest = [NSURLRequest requestWithURL:departuresEndpointURL];
+    
+    NSURLSessionDataTask* departureDownloadTask = [departuresDownloadSession dataTaskWithRequest:departuresInfoRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        [self setRepresentedObject:[self parseDepartureData:data]];
+    }];
+    
+    [departureDownloadTask resume];
+}
 
-    // Update the view, if already loaded.
+- (NSArray *)parseDepartureData:(NSData *)downloadData
+{
+    NSString* dataAsString = [[NSString alloc] initWithData:downloadData encoding:NSUTF8StringEncoding];
+    
+    NSArray* dataInLines = [dataAsString componentsSeparatedByString:@"\r"];
+    NSArray* departureStrings = [dataInLines subarrayWithRange:NSMakeRange(1, [dataInLines count] - 1)];
+    
+    NSMutableArray* parsedDepartures = [NSMutableArray arrayWithCapacity:[departureStrings count]];
+    for (NSString* departureString in departureStrings) {
+        NSString* trimmedDepartureString = [departureString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        NSDictionary* parsedDeparture = [self parseDepartureString:trimmedDepartureString];
+        [parsedDepartures addObject:parsedDeparture];
+    }
+    
+    return parsedDepartures;
+}
+
+- (NSDictionary *)parseDepartureString:(NSString *)departureString
+{
+    NSMutableDictionary* departure = [NSMutableDictionary dictionary];
+
+    NSError* jsonError;
+    NSArray* departureComponents = [NSJSONSerialization JSONObjectWithData:[departureString dataUsingEncoding:NSUTF8StringEncoding] options:0 error:&jsonError];
+    
+    [departure setObject:departureComponents[3] forKey:@"line"];
+    [departure setObject:departureComponents[4] forKey:@"direction"];
+
+    
+    NSNumber* departureTimeInPosixTime = departureComponents[5];
+    NSDate* departureDate = [NSDate dateWithTimeIntervalSince1970:[departureTimeInPosixTime longValue]/1000];
+
+    [departure setObject:departureDate forKey:@"departure"];
+    
+    return departure;
 }
 
 @end
