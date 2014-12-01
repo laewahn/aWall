0a0e357930068d0a7a3e8ebc031edb41d693b83c
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2014-12-01 20:21:20 +0100

    New target for menu icon app

---
+++ iSeag MenuIcon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iSeag MenuIcon
+//
+//  Created by Dennis Lewandowski on 01/12/14.
+//  Copyright (c) 2014 laewahn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ iSeag MenuIcon/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  iSeag MenuIcon
+//
+//  Created by Dennis Lewandowski on 01/12/14.
+//  Copyright (c) 2014 laewahn. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
